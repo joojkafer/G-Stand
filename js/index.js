cb4a0c8d0df225d1e24b56de17f70adfd5a4e8ed
joojkafer <joaoantoniokaferribeiro@gmail.com>
2023-10-12 00:49:49 -0300

    add js na main

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function sweetSuccess(){
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Alterações salvas com sucesso!',
+      })
+}
--- teste.js
+++
@@ -1,8 +0,0 @@
-function sweet(){
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-        footer: '<a href="">Why do I have this issue?</a>'
-      })
-}
