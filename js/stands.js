bb35b7a50cc4c6fd91838f3c955a09a9a6d6c7d3
joojkafer <joaoantoniokaferribeiro@gmail.com>
2023-10-30 00:04:25 -0300

    concluido stands.html

--- js/stands.js
+++ js/stands.js
@@ -19,3 +19,41 @@ $(document).ready(function(){
 		}
 	});
 });
+
+function addStandSuccess(){
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+		  toast.addEventListener('mouseenter', Swal.stopTimer)
+		  toast.addEventListener('mouseleave', Swal.resumeTimer)
+		}
+	})
+	  
+	Toast.fire({
+		icon: 'success',
+		title: 'Adicionado com sucesso!'
+	})
+}
+
+function clearStandSuccess(){
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+		  toast.addEventListener('mouseenter', Swal.stopTimer)
+		  toast.addEventListener('mouseleave', Swal.resumeTimer)
+		}
+	})
+	  
+	Toast.fire({
+		icon: 'success',
+		title: 'Dados deletados com sucesso!'
+	})
+}
